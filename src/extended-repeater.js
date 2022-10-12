a37b219dedccd2191286d26de6978b9d7324c296
Viktoria <aliceinapple99@gmail.com>
2022-10-12 16:29:19 +0300

    feat: solve extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,53 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatTimes, separator, addition, additionRepeatTimes, additionSeparator;
+
+  str = String(str);
+  options["addition"] !== undefined
+    ? (addition = String(options["addition"]))
+    : (addition = "");
+
+  options["repeatTimes"]
+    ? (repeatTimes = options["repeatTimes"])
+    : (repeatTimes = 1);
+
+  options["additionRepeatTimes"]
+    ? (additionRepeatTimes = options["additionRepeatTimes"])
+    : (additionRepeatTimes = 1);
+
+  options["separator"] ? (separator = options["separator"]) : (separator = "+");
+  options["additionSeparator"]
+    ? (additionSeparator = options["additionSeparator"])
+    : (additionSeparator = "|");
+
+  let arr = [];
+  let resultArr = [];
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    arr.push(addition);
+  }
+  let additionStr = arr.join(additionSeparator);
+  for (let i = 0; i < repeatTimes; i++) {
+    resultArr.push(str + additionStr);
+  }
+  return resultArr.join(separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
