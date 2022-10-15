3aa45b2863db38bafdf7a37444a0acabee61e822
Viktoria <aliceinapple99@gmail.com>
2022-10-15 12:08:13 +0300

    feat: solve mine-sweeper.js

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  copyMatrix = JSON.parse(JSON.stringify(matrix));
+  for (let i = 0; i < matrix.length; i++) {
+    let count = 0;
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrix[i][j - 1] ? count++ : 0;
+      matrix[i - 1] ? (matrix[i - 1][j - 1] ? count++ : 0) : 0;
+      matrix[i - 1] ? (matrix[i - 1][j] ? count++ : 0) : 0;
+      matrix[i - 1] ? (matrix[i - 1][j + 1] ? count++ : 0) : 0;
+      matrix[i][j + 1] ? count++ : 0;
+      matrix[i + 1] ? (matrix[i + 1][j + 1] ? count++ : 0) : 0;
+      matrix[i + 1] ? (matrix[i + 1][j] ? count++ : 0) : 0;
+      matrix[i + 1] ? (matrix[i + 1][j - 1] ? count++ : 0) : 0;
+      copyMatrix[i][j] = count;
+      count = 0;
+    }
+  }
+  return copyMatrix;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
