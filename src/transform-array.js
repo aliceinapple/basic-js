18b5643167efe5e402b5a473d6a1b7b8e879bc4b
Viktoria <aliceinapple99@gmail.com>
2022-10-15 14:26:37 +0300

    feat: solve transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,19 @@
-const { NotImplementedError } = require("../extensions/index.js");
+const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- *
+ * 
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- *
+ * 
  * @example
- *
+ * 
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- *
+ * 
  */
-function transform(arr) {
+ function transform(arr) {
   if (!Array.isArray(arr))
     throw new Error("'arr' parameter must be an instance of the Array!");
   let newArr = Array.from(arr);
@@ -21,12 +21,12 @@ function transform(arr) {
   for (let i = 0; i <= arr.length; i++) {
     if (arr[i] === "--discard-next") {
       newArr[newArr.indexOf("--discard-next") + 1]
-        ? newArr.splice(newArr.indexOf("--discard-next"), 2)
-        : newArr.splice(newArr.indexOf("--discard-next"), 1);
+        ? newArr.splice(newArr.indexOf("--discard-next"), 2, null)
+        : newArr.splice(newArr.indexOf("--discard-next"), 1, null);
     } else if (arr[i] === "--discard-prev") {
       newArr[newArr.indexOf("--discard-prev") - 1]
-        ? newArr.splice(newArr.indexOf("--discard-prev") - 1, 2)
-        : newArr.splice(newArr.indexOf("--discard-prev"), 1);
+        ? newArr.splice(newArr.indexOf("--discard-prev") - 1, 2, null)
+        : newArr.splice(newArr.indexOf("--discard-prev"), 1, null);
     } else if (arr[i] === "--double-next") {
       newArr[newArr.indexOf("--double-next") + 1]
         ? newArr.splice(
@@ -45,12 +45,10 @@ function transform(arr) {
         : newArr.splice(newArr.indexOf("--double-prev"), 1);
     }
   }
-  console.log(arr);
-  return newArr;
-}
-
-transform(arr)
+  
+  return newArr.filter(item => item !== null);
+};
 
 module.exports = {
-  transform,
+  transform
 };
